c7e3b02e73449031c75b4786c78572466f33202d
John Thekkudan <162726539+JohnThekku@users.noreply.github.com>
2026-05-18 03:36:27 +0400

    Change Discord intents to allow all events

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from discord.ext import commands
 from datetime import timedelta
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix="s!", intents=intents)
 
